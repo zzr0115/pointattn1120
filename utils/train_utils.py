8f5e22a3693b7f3548e1db904e14a8a86b492d5e
zzr <@163.com>
2025-11-20 21:44:01 +0800

    init

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -27,11 +27,15 @@ def set_requires_grad(nets, requires_grad=False):
 
 
 def save_model(path, net, net_d=None):
+    # 处理 DataParallel/DistributedDataParallel 包装的模型
+    net_state = net.module.state_dict() if hasattr(net, 'module') else net.state_dict()
+    
     if net_d is not None:
-        torch.save({'net_state_dict': net.module.state_dict(),
-                    'D_state_dict': net_d.module.state_dict()}, path)
+        net_d_state = net_d.module.state_dict() if hasattr(net_d, 'module') else net_d.state_dict()
+        torch.save({'net_state_dict': net_state,
+                    'D_state_dict': net_d_state}, path)
     else:
-        torch.save({'net_state_dict': net.module.state_dict()}, path)
+        torch.save({'net_state_dict': net_state}, path)
 
 
 
